7f4ddc7a2d5a5aaf345dd24f8780544be3aa67a0
MAFTOUH <omar.maftou0000@gmail.com>
2024-01-27 00:27:55 +0100

    numpy reshape

--- numPy/Slicing.py
+++ numPy/Slicing.py
@@ -1,8 +1,22 @@
 import numpy as np
-import sys
-import time
 
 myList01 = [[1 , 2 , 3] , [4 , 5 , 7] , [8 , 9 , 10] , [11 , 12 , 13]]
 myArray = np.array(myList01)
 
-print(myArray[0: , :2])
+# print(myArray[0: , :2])
+
+myArray02 = np.array([1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12])
+print(myArray02.ndim) # 1
+print(myArray02.shape) # (12 ,)
+
+print("*" * 50)
+
+reshape_myArray02 = myArray02.reshape(2 , 6)
+print(reshape_myArray02.ndim) # 2
+print(reshape_myArray02)  # [ [ 1  2  3  4  5  6] , [ 7  8  9 10 11 12] ]
+
+print("*" * 50)
+
+reshape02_myArray02 = myArray02.reshape(2 , 2 , 3)
+print(reshape02_myArray02.ndim) # 3
+print(reshape02_myArray02)  # [ [[ 1  2  3] , [ 4  5  6]] , [[ 7  8  9] , [10 11 12]] ]
